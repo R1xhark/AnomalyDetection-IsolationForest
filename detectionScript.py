966f56bafbad1490d681b254686058cde1e59c81
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-14 14:25:29 +0200

    Update detectionScript.py

--- detectionScript.py
+++ detectionScript.py
@@ -7,20 +7,20 @@ def load_data(file_path):
     return data
 
 def preprocess_data(data):
-    collums_to_drop = ['collums_to_drop1', 'collums_to_drop2']
-    data = data.drop(columns=collums_to_drop)
+    columns_to_drop = ['column_to_drop1', 'column_to_drop2']
+    data = data.drop(columns=columns_to_drop)
     
     data.fillna(0, inplace=True)
 
     from sklearn.preprocessing import StandardScaler
     scaler = StandardScaler()
-    numerické_sloupce = ['numeric_attribute_1', 'numeric_attribute_2']
-    data[numerické_sloupce] = scaler.fit_transform(data[numerické_sloupce])
+    numeric_columns = ['numeric_attribute_1', 'numeric_attribute_2']
+    data[numeric_columns] = scaler.fit_transform(data[numeric_columns])
 
-    kategorické_sloupce = ['categorical_attribute']
-    data = pd.get_dummies(data, columns=categorical_attribute, drop_first=True)
+    categorical_columns = ['categorical_attribute']
+    data = pd.get_dummies(data, columns=categorical_columns, drop_first=True)
 
-    data['nový_příznak'] = data['numeric_attribute_1'] + data['numeric_attribute_1']
+    data['combined_numeric_features'] = data['numeric_attribute_1'] + data['numeric_attribute_2']
 
     return data
 
@@ -31,20 +31,19 @@ def detect_anomalies(data):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", type=str, help="Cesta k CSV souboru obsahujícímu bezpečnostní data")
+    parser.add_argument("file_path", type=str, help="Path to the CSV file containing security data")
     args = parser.parse_args()
 
     file_path = args.file_path
     data = load_data(file_path)
-    print("Původní Data:")
+    print("Original Data:")
     print(data.head())
 
     data = preprocess_data(data)
-    print("\nZpracovaná Data:")
+    print("\nPreprocessed Data:")
     print(data.head())
 
     data = detect_anomalies(data)
     anomalies = data[data['anomaly'] == -1]
-    print("\nDetekované Anomalie:")
+    print("\nAnomalies Detected:")
     print(anomalies)
-
